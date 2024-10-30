1d2326b21e4c7e73804d6954c794ab73f6b5d382
Jose Miguel-Alonso <106067594+acpmialj@users.noreply.github.com>
2024-10-30 15:49:49 +0100

    Update launch_impala.sh

--- launch_impala.sh
+++ launch_impala.sh
@@ -1,3 +1,3 @@
 docker run -d --name kudu-impala --network=kudu-impala_default \
   -p 21000:21000 -p 21050:21050 -p 25000:25000 -p 25010:25010 -p 25020:25020 \
-  --memory=4096m apache/kudu:impala-latest impala
+  --memory=4096m -e JAVA_HOME="/usr" apache/kudu:impala-latest impala
